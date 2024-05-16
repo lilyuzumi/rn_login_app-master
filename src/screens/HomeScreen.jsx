93cc1b4bf1cc025a18d329c9895987b3a59e94fe
lily <lilyuzmi@gmail.com>
2024-05-16 15:54:23 -0300

    first commit

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Bem vinda(o) ao nosso app</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("LoginScreen");
+        }}
+        mode="contained"
+      >
+        Login
+      </Button>
+    </View>
+  );
+}
